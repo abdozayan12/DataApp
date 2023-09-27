9f378bf321e80b9774c242c10f9f3ee722ff8695
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-09-27 19:36:34 +0300

    update

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :likes
-    has_many :comments
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
 end
