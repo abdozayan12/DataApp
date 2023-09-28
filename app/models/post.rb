00adc2f4923cafd7467c0dd046cf828bc0e79ac2
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-09-28 11:50:01 +0300

    add data process

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+
+  after_save :update_posts_counter
+  private
+  def update_posts_counter
+    author.increment!(:post_counter)
+  end
 end
