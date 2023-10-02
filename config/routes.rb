d5f48bc636453c119a7627187fae48c6fe853703
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-02 18:32:53 +0300

    fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  root "users#index"
+  root 'users#index'
 
-  get "/users/:user_id/posts/:id", to: "posts#index", as: "user_post"
-  get "/users/:user_id/posts", to: "posts#show", as: "user_posts"
-  get "/users/:id", to: "users#show", as: "user"
-  get "/users", to: "users#index", as: "users"
+  get '/users/:user_id/posts/:id', to: 'posts#index', as: 'user_post'
+  get '/users/:user_id/posts', to: 'posts#show', as: 'user_posts'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users', to: 'users#index', as: 'users'
 end
