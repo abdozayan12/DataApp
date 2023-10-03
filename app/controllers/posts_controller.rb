7322063e5195be04cbe3ca1e83b85e1a6063cbce
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-03 16:57:46 +0300

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
 
   def show
-    puts params
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
   end
 end
