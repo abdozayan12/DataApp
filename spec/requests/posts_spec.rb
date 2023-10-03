45c5f5f24c75e743bdbdf9321dced3c5b2b38002
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-03 14:16:00 +0300

    Update posts_spec.rb

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/posts/index'
+  describe 'GET /users/:user_id/posts/:id' do
+    it 'returns http success, renders the show template, and includes correct placeholder text' do
+      get '/users/1/posts/1'
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+      expect(response.body).to include("Placeholder text for show view")
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/posts/show'
+  describe 'GET /users/:user_id/posts' do
+    it 'returns http success, renders the index template, and includes correct placeholder text' do
+      get '/users/1/posts'
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+      expect(response.body).to include("Placeholder text for index view")
     end
   end
 end
