27e969f30b415d99b20b5777887e072c3763f8fe
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-03 19:13:22 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'users#index'
 
   # Posts routes
-  get '/users/:user_id/posts/:id', to: 'posts#index', as: 'user_post'
-  get '/users/:user_id/posts', to: 'posts#show', as: 'user_posts'
+  get '/users/:user_id/posts/:id', to: 'posts#index', as: 'user_post_index'
+  get '/users/:user_id/posts', to: 'posts#show', as: 'user_posts_show'
 
   # Users routes
   get "/users/:user_id/posts/:id", to: "users#show", as: "user_post"
