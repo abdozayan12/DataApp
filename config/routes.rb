921ca301f89a49adcc7d912ff2355c575534391f
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-04 17:54:33 +0300

    fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  root "users#index"
+  root 'users#index'
 
-  get "/users/:id", to: "users#show", as: "user"
-  get "/users", to: "users#index", as: "users"
-  post "/users/:user_id/posts/:id/newcomment", to: "comments#create", as: "create_new_comment"
-  post "/users/:user_id/posts/:id/newlike", to: "likes#create", as: "create_new_like"
-  get "/users/:user_id/posts/new", to: "posts#new", as: "user_new_posts"
-  post "/users/:user_id/posts/new", to: "posts#create", as: "create_user_posts"
-  get "/users/:user_id/posts/:id", to: "posts#show", as: "user_post"
-  get "/users/:user_id/posts", to: "posts#index", as: "user_posts"
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users', to: 'users#index', as: 'users'
+  post '/users/:user_id/posts/:id/newcomment', to: 'comments#create', as: 'create_new_comment'
+  post '/users/:user_id/posts/:id/newlike', to: 'likes#create', as: 'create_new_like'
+  get '/users/:user_id/posts/new', to: 'posts#new', as: 'user_new_posts'
+  post '/users/:user_id/posts/new', to: 'posts#create', as: 'create_user_posts'
+  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
 end
