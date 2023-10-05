b0cf7c1140ff079d737684a3f1b1594a8cf6f8ab
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-05 10:37:00 +0300

    fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'users#index'
 
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :new, :create, :show] do
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index new create show] do
       resources :comments, only: [:create]
       resources :likes, only: [:create]
     end
-  end  
+  end
 end
