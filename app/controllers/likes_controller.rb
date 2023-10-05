6fb293bafc66b1730850de88ae691c8a853da927
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-05 10:29:55 +0300

    update

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,12 @@
-class LikesController < ApplicationController
-  def create
-    @post = Post.find(params[:id])
-    @like = Like.create(user: current_user, post: @post)
+def create
+  @post = Post.find(params[:post_id])
+  @like = Like.new(user: current_user, post: @post)
 
-    if @like.persisted?
-      flash[:success] = 'Liked successfully!'
-      redirect_to user_post_path(current_user, @post)
-    else
-      flash[:alert] = 'Something went wrong'
-      render :new
-    end
+  if @like.save
+    flash[:success] = 'Liked successfully!'
+    redirect_to user_post_path(@post.user, @post)
+  else
+    flash[:alert] = 'Something went wrong'
+    render :new
   end
 end
