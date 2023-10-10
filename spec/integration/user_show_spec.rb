dc44e91971b8f5c6412c35fdba33f265ded27440
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-10 16:50:20 +0300

    Update user_show_spec.rb

--- spec/integration/user_show_spec.rb
+++ spec/integration/user_show_spec.rb
@@ -33,4 +33,49 @@ RSpec.describe 'users#show', type: :feature do
 
     visit users_url
   end
+  describe '#show page' do
+    before(:each) do
+      visit user_path(@user1)
+    end
+
+    it 'can see the user profile picture' do
+      expect(page).to have_css("img[src='#{@user1.photo}']")
+    end
+
+    it 'can see the user username' do
+      expect(page).to have_content(@user1.name.to_s)
+    end
+
+    it 'can see the number of posts the user has written' do
+      expect(page).to have_content(@user1.posts_counter.to_s)
+    end
+
+    it 'can see the user bio' do
+      expect(page).to have_content(@user1.bio.to_s)
+    end
+
+    it 'can see the user first 3 posts' do
+      @user1.recent_posts.each do |post|
+        expect(page).to have_content(post.text)
+      end
+    end
+
+  it 'can see a button to view all user posts' do
+      expect(page).to have_selector('button', text: 'See all posts')
+    end
+
+    it 'redirects to the user posts index page when "See all posts" button is clicked'  do
+      visit user_path(@user1)
+      click_link 'See all posts'
+      expect(page).to have_current_path(user_posts_path(@user1))
+    end
+  end
+  describe 'GET/posts/show' do
+    it ',redirects me to post show page, when I clicked a user post' do
+      visit user_path(@user1)
+      post = @user1.recent_posts.first
+      click_link(post.title)
+      expect(page).to have_current_path(user_post_path(@user1, post.id))
+    end
+  end
 end
