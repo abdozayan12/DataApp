1e724e5153cf6904addf0a8fb8f034d549e876ab
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-11 18:30:43 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_928_125_838) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_11_150142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,18 +33,6 @@ ActiveRecord::Schema[7.0].define(version: 20_230_928_125_838) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table 'models', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_models_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_models_on_reset_password_token', unique: true
-  end
-
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -55,18 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 20_230_928_125_838) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_models_on_email", unique: true
     t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-  end
-
-  create_table 'models', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_models_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_models_on_reset_password_token', unique: true
   end
 
   create_table "posts", force: :cascade do |t|
