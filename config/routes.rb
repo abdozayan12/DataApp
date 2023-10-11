cdac6b5f9e0d59e43dc2dc0362e73009b474673b
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-11 11:45:52 +0300

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :models
   root to: 'users#index'
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
