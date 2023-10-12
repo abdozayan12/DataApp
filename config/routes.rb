05bbc8178bcfbde3031d2571b5adfbd103580ae2
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-12 11:19:35 +0300

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :models
-  root to: 'users#index'
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
-      resources :comments, only: %i[create new]
-      resources :likes, only: %i[create new]
-      get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
-      delete '/users/:user_id/posts/:post_id/comments/:id', to: 'comments#destroy', as: 'delete_comment'
-      delete '/users/:user_id/posts/:id', to: 'posts#destroy', as: 'delete_post'
+  root to: "users#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:create, :new]
+       resources :likes, only: [:create, :new] 
     end
   end
 end
